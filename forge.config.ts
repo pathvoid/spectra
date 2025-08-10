72da23ab4c90857932f715f149bc92d832477cfb
pathvoid <accounts@pathvoid.com>
2025-08-10 08:37:37 -0400

    fix: enable windows builds in forge and workflows.

--- forge.config.ts
+++ forge.config.ts
@@ -23,7 +23,7 @@ const config: ForgeConfig = {
     }),
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin', 'win32']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
